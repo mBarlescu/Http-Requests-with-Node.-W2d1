9d784bf59b45b0892267a5842af77f0310749b37
Mark Barlescu <barlescu92@gmail.com>
2018-10-29 19:08:51 +0000

    step 2, no more line breaks between chunks

--- printChunks.js
+++ printChunks.js
@@ -2,26 +2,47 @@ var https = require('https');
 
 function getAndPrintHTMLChunks (){
 
-var requestOptions = {
-  host: 'sytantris.github.io',
-  path: '/http-examples/step1.html'
-};
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
 
-https.get(requestOptions, function (response){
-  response.setEncoding('utf8');
+  https.get(requestOptions, function (response){
+    response.setEncoding('utf8');
 
-  response.on('data', function (data) {
-    console.log('Chunk received. Length:', data)
+    response.on('data', function (data) {
+      console.log('Chunk received. Length:', data)
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+
   });
-
-  response.on('end', function() {
-    console.log('Response stream complete.');
-  });
-
-});
-
-
 
 }
 
-getAndPrintHTMLChunks()
+function getAndPrintHTML () {
+
+  var body = '';
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function (response){
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      body += data;
+      console.log(body);
+    });
+
+    response.on('end', function () {
+      console.log('Response stream complete')
+    });
+  });
+}
+
+getAndPrintHTML()
